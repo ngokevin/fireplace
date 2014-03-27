4b1f905a97a543c6f0f228d20470f08fb92fe2e5
Kevin Ngo <ngoke@mozilla.com>
2014-03-27 01:57:41 -0800

    defer image loading (bug 898320)

--- hearth/media/js/marketplace.js
+++ hearth/media/js/marketplace.js
@@ -199,6 +199,30 @@ function(_) {
         require('storage').setItem('hide_incompatibility_banner', true);
     });
 
+    var ImageDeferrer = require('image-deferrer');
+    var iconDeferrer = ImageDeferrer.Deferrer(100, null);
+    var screenshotDeferrer = ImageDeferrer.Deferrer(null, 200);
+    z.page.one('loaded', function() {
+        iconDeferrer.setImages($('.icon.deferred'));
+        screenshotDeferrer.setImages($('.screenshot img.deferred'));
+    }).on('loaded loaded_more navigate', function() {
+        iconDeferrer.refresh();
+        screenshotDeferrer.refresh();
+    });
+    nunjucks_globals.imgAlreadyDeferred = function(src) {
+        /*
+            If an image already has been loaded, we use this helper in case the
+            view is triggered to be rebuilt. When pages are rebuilt, we don't
+            mark images to be deferred if they have already been loaded.
+            This fixes images flashing back to the placeholder image when
+            switching between the New and Popular tabs on the home page.
+        */
+        var iconsLoaded = iconDeferrer.getSrcsAlreadyLoaded();
+        var screenshotsLoaded = screenshotDeferrer.getSrcsAlreadyLoaded();
+        var loaded = iconsLoaded.concat(screenshotsLoaded);
+        return loaded.indexOf(src) !== -1;
+    };
+
     window.addEventListener(
         'resize',
         _.debounce(function() {z.doc.trigger('saferesize');}, 200),
