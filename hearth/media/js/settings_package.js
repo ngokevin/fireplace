1112fb30740e4213a7b4f98127f9d3da345a3f55
Kevin Ngo <ngoke@mozilla.com>
2014-04-18 16:10:59 -0700

    stuff

--- hearth/media/js/settings_package.js
+++ hearth/media/js/settings_package.js
@@ -2,9 +2,9 @@ define('settings_local', [], function() {
     // TODO: Allow us to change languages, media url, languages from the settings
 
     return {
-        api_url: 'https://marketplace-dev.allizom.org',
+        api_url: 'https://marketplace.firefox.com',
         body_classes: 'package',
-        media_url: 'https://marketplace-dev.mozflare.net/media/',
+        media_url: 'https://marketplace.cdn.mozilla.net/',
         tracking_enabled: true,
         potatolytics_enabled: true,
         package_version: '{fireplace_package_version}'
